d35b54fd7b0360cd5d4b9bdcdf29163c45ee10d1
GitHub Action <action@github.com>
2022-04-19 03:25:39 +0000

    Template cleanup

--- src/main/kotlin/com/github/wangdingfu/fuapidocplugin/services/MyApplicationService.kt
+++ src/main/kotlin/com/github/wangdingfu/fuapidocplugin/services/MyApplicationService.kt
@@ -1,6 +1,6 @@
-package org.jetbrains.plugins.template.services
+package com.github.wangdingfu.fuapidocplugin.services
 
-import org.jetbrains.plugins.template.MyBundle
+import com.github.wangdingfu.fuapidocplugin.MyBundle
 
 class MyApplicationService {
 
--- src/main/kotlin/com/github/wangdingfu/fuapidocplugin/services/MyProjectService.kt
+++ src/main/kotlin/com/github/wangdingfu/fuapidocplugin/services/MyProjectService.kt
@@ -1,7 +1,7 @@
-package org.jetbrains.plugins.template.services
+package com.github.wangdingfu.fuapidocplugin.services
 
 import com.intellij.openapi.project.Project
-import org.jetbrains.plugins.template.MyBundle
+import com.github.wangdingfu.fuapidocplugin.MyBundle
 
 class MyProjectService(project: Project) {
 
