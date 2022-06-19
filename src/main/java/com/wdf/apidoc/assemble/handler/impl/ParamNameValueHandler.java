6d75acee5931cf5527287d5a7d1a51526f2e4205
wangdingfu <wangdingfu1024@163.com>
2022-06-19 22:20:17 +0800

    校验注解工具类新增 支持获取校验注解message信息作为参数名

--- src/main/java/com/wdf/apidoc/assemble/handler/impl/ParamNameValueHandler.java
+++ src/main/java/com/wdf/apidoc/assemble/handler/impl/ParamNameValueHandler.java
@@ -1,11 +1,10 @@
 package com.wdf.apidoc.assemble.handler.impl;
 
-import com.thaiopensource.relaxng.translate.util.Param;
 import com.wdf.apidoc.assemble.handler.BaseParamFieldValueHandler;
-import com.wdf.apidoc.assemble.handler.ParamValueHandler;
 import com.wdf.apidoc.constant.enumtype.ParamValueType;
-import com.wdf.apidoc.pojo.desc.BaseInfoDesc;
 import com.wdf.apidoc.pojo.desc.ObjectInfoDesc;
+import com.wdf.apidoc.util.ValidateAnnotationUtils;
+import org.apache.commons.lang3.StringUtils;
 
 /**
  * @Author wangdingfu
@@ -22,13 +21,20 @@ public class ParamNameValueHandler extends BaseParamFieldValueHandler {
 
     @Override
     protected String doGetParamValue(ObjectInfoDesc objectInfoDesc) {
-
         //获取swagger注解标识的参数名
 
         //获取注释参数名
-
+        String name = objectInfoDesc.getName();
+        if (StringUtils.isNotBlank(name)) {
+            return name;
+        }
         //获取校验注解信息截取出字段名
+        String validMessage = ValidateAnnotationUtils.getValidMessage(objectInfoDesc);
+        if (StringUtils.isNotBlank(validMessage)) {
+            return validMessage.replace("不能为空", "");
+        }
         return "";
     }
+
 
 }
---
+++ src/main/java/com/wdf/apidoc/util/ValidateAnnotationUtils.java
@@ -0,0 +1,62 @@
+package com.wdf.apidoc.util;
+
+import com.wdf.apidoc.constant.AnnotationConstants;
+import com.wdf.apidoc.pojo.data.AnnotationData;
+import com.wdf.apidoc.pojo.desc.BaseInfoDesc;
+import org.apache.commons.lang3.StringUtils;
+
+import java.util.Optional;
+
+/**
+ * @Author wangdingfu
+ * @Description 校验注解工具类
+ * @Date 2022-06-18 22:55:28
+ */
+public class ValidateAnnotationUtils {
+
+
+    /**
+     * 返回参数是否必填
+     *
+     * @param baseInfoDesc 描述对象抽象类
+     * @return 是|否
+     */
+    public static String isRequire(BaseInfoDesc baseInfoDesc) {
+        Optional<AnnotationData> annotation = baseInfoDesc.getAnnotation(AnnotationConstants.VALID_NOT);
+        return annotation.isPresent() ? "是" : "否";
+    }
+
+
+    /**
+     * 获取校验注解Message信息
+     *
+     * @param baseInfoDesc 描述对象抽象类
+     * @return message内容
+     */
+    public static String getValidMessage(BaseInfoDesc baseInfoDesc) {
+        if (baseInfoDesc.exists(AnnotationConstants.VALID_NOT_NULL)) {
+            return getMessageValue(baseInfoDesc, AnnotationConstants.VALID_NOT_NULL);
+        }
+
+        if (baseInfoDesc.exists(AnnotationConstants.VALID_NOT_BLANK)) {
+            return getMessageValue(baseInfoDesc, AnnotationConstants.VALID_NOT_BLANK);
+        }
+
+        if (baseInfoDesc.exists(AnnotationConstants.VALID_NOT_EMPTY)) {
+            return getMessageValue(baseInfoDesc, AnnotationConstants.VALID_NOT_EMPTY);
+        }
+
+        return StringUtils.EMPTY;
+    }
+
+
+    private static String getMessageValue(BaseInfoDesc baseInfoDesc, String annotationName) {
+        Optional<AnnotationData> annotation = baseInfoDesc.getAnnotation(annotationName);
+        if (annotation.isPresent()) {
+            return annotation.get().getValue("message").getStringValue();
+        }
+        return StringUtils.EMPTY;
+    }
+
+
+}
