bbf8ab6d2fb674073dcfa7220df3e3ffc5a2dcd1
wangdingfu <fuge@douyu.tv>
2022-06-21 22:17:52 +0800

    新增swagger注解支持（ApiModelProperty、ApiParam）

--- src/main/java/com/wdf/apidoc/assemble/handler/impl/ParamNameValueHandler.java
+++ src/main/java/com/wdf/apidoc/assemble/handler/impl/ParamNameValueHandler.java
@@ -28,11 +28,21 @@ public class ParamNameValueHandler extends BaseParamFieldValueHandler {
     @Override
     protected String doGetParamValue(ObjectInfoDesc objectInfoDesc) {
         //获取swagger注解标识的参数名
+
+        //ApiModelProperty注解支持
         Optional<AnnotationData> annotation = objectInfoDesc.getAnnotation(AnnotationConstants.SWAGGER_API_MODEL_PROPERTY);
         String swaggerName = AnnotationUtils.getAnnotationValue(annotation, FuDocConstants.VALUE, FuDocConstants.AnnotationAttr.NAME);
         if (StringUtils.isNotBlank(swaggerName)) {
             return swaggerName;
         }
+
+        //ApiParam注解支持
+        annotation = objectInfoDesc.getAnnotation(AnnotationConstants.SWAGGER_API_PARAM);
+        swaggerName = AnnotationUtils.getAnnotationValue(annotation, FuDocConstants.AnnotationAttr.NAME, FuDocConstants.VALUE);
+        if (StringUtils.isNotBlank(swaggerName)) {
+            return swaggerName;
+        }
+
         //获取注释参数名
         String name = objectInfoDesc.getName();
         if (StringUtils.isNotBlank(name)) {
@@ -45,6 +55,5 @@ public class ParamNameValueHandler extends BaseParamFieldValueHandler {
         }
         return "";
     }
-
 
 }
