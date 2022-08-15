9d04a17368b51e902cdbade6d79d41c8b9494eed
wangdingfu <fuge@douyu.tv>
2022-08-15 20:07:20 +0800

    测试访问外部接口

--- src/main/java/com/wdf/fudoc/listener/FuDocAppStartUpListener.java
+++ src/main/java/com/wdf/fudoc/listener/FuDocAppStartUpListener.java
@@ -1,9 +1,11 @@
 package com.wdf.fudoc.listener;
 
 
-import com.intellij.openapi.application.ApplicationActivationListener;
+import cn.hutool.http.HttpUtil;
 import com.intellij.openapi.diagnostic.Logger;
-import com.intellij.openapi.wm.IdeFrame;
+import com.intellij.openapi.project.DumbAware;
+import com.intellij.openapi.project.Project;
+import com.intellij.openapi.startup.StartupActivity;
 import com.wdf.fudoc.FuDocNotification;
 import com.wdf.fudoc.view.FuDocGeneralForm;
 import org.jetbrains.annotations.NotNull;
@@ -13,12 +15,14 @@ import org.jetbrains.annotations.NotNull;
  * @author wangdingfu
  * @date 2022-08-14 22:32:40
  */
-public class FuDocApplicationActivationListener implements ApplicationActivationListener {
+public class FuDocAppStartUpListener implements StartupActivity, DumbAware {
     private static final Logger LOGGER = Logger.getInstance(FuDocGeneralForm.class);
 
     @Override
-    public void applicationActivated(@NotNull IdeFrame ideFrame) {
+    public void runActivity(@NotNull Project project) {
         LOGGER.info("应用已经启动了");
+        String result = HttpUtil.get("https://www.baidu.com");
+        LOGGER.info(result);
         FuDocNotification.notifyInfo("【Fu Doc】发布了新版本. 本次新增了许多有用功能和修复了很多历史bug. 快去插件市场升级最新版本吧!");
     }
 }
