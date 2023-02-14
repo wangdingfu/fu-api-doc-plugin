2909471215a13a29c6ee988169f0f53f909e9b34
wangdingfu <wangdingfu1024@163.com>
2023-02-14 22:19:42 +0800

    bug修复

--- src/main/java/com/wdf/fudoc/test/action/TestAction.java
+++ src/main/java/com/wdf/fudoc/test/action/TestAction.java
@@ -1,8 +1,11 @@
 package com.wdf.fudoc.test.action;
 
+import com.intellij.ide.highlighter.JavaFileType;
 import com.intellij.openapi.actionSystem.*;
+import com.intellij.openapi.editor.impl.EditorComponentImpl;
 import com.intellij.util.ui.UIUtil;
 import com.wdf.fudoc.apidoc.sync.data.YApiProjectTableData;
+import com.wdf.fudoc.components.FuEditorComponent;
 import com.wdf.fudoc.components.FuTableComponent;
 import com.wdf.fudoc.components.factory.FuTableColumnFactory;
 import com.wdf.fudoc.request.pojo.FuHttpRequestData;
@@ -17,16 +20,25 @@ import java.util.concurrent.atomic.AtomicBoolean;
 public class TestAction extends AnAction {
 
 
+
+
     @Override
     public void actionPerformed(@NotNull AnActionEvent e) {
-        FuRequestStatusInfoView fuRequestStatusInfoView = new FuRequestStatusInfoView();
-        FuHttpRequestData fuHttpRequestData = new FuHttpRequestData();
-        fuHttpRequestData.setHttpCode(200);
-        fuHttpRequestData.setTime(1045L);
-        fuRequestStatusInfoView.initData(fuHttpRequestData);
-        JPanel rootPanel = fuRequestStatusInfoView.getRootPanel();
-        UIUtil.applyStyle(UIUtil.ComponentStyle.SMALL, rootPanel);
-        PopupUtils.create(rootPanel,null,new AtomicBoolean(true));
+        FuEditorComponent fuEditorComponent = FuEditorComponent.create(JavaFileType.INSTANCE);
+        fuEditorComponent.setContent("package com.wdf.fudoc;\n" +
+                "\n" +
+                "/**\n" +
+                " * @author wangdingfu\n" +
+                " * @date 2023-02-10 22:16:48\n" +
+                " */\n" +
+                "public class FuDocTest {\n" +
+                "\n" +
+                "\n" +
+                "    public void authAfter(String userName, String password) {\n" +
+                "\n" +
+                "    }\n" +
+                "}\n");
+        PopupUtils.create(fuEditorComponent.getMainPanel(),null,new AtomicBoolean(true));
     }
 
 
--- src/main/java/com/wdf/fudoc/test/action/TestTableAction.java
+++ src/main/java/com/wdf/fudoc/test/action/TestTableAction.java
@@ -1,20 +1,44 @@
 package com.wdf.fudoc.test.action;
 
+import com.intellij.ide.highlighter.JavaFileType;
 import com.intellij.openapi.actionSystem.AnAction;
 import com.intellij.openapi.actionSystem.AnActionEvent;
+import com.intellij.openapi.editor.Document;
+import com.intellij.openapi.editor.Editor;
+import com.intellij.openapi.editor.EditorFactory;
 import com.intellij.openapi.module.Module;
 import com.intellij.openapi.module.ModuleUtil;
+import com.intellij.openapi.util.io.FileUtil;
+import com.intellij.openapi.vfs.VirtualFile;
+import com.intellij.pom.java.LanguageLevel;
+import com.intellij.psi.*;
+import com.intellij.psi.impl.PsiFileFactoryImpl;
+import com.intellij.psi.impl.PsiManagerImpl;
+import com.intellij.psi.impl.file.PsiDirectoryFactory;
+import com.intellij.psi.impl.file.PsiFileImplUtil;
+import com.intellij.psi.impl.file.impl.FileManager;
+import com.intellij.psi.impl.file.impl.FileManagerImpl;
+import com.intellij.psi.impl.source.PsiJavaFileImpl;
+import com.intellij.psi.impl.source.resolve.reference.impl.providers.PsiFileSystemItemUtil;
+import com.intellij.psi.util.FindClassUtil;
+import com.intellij.psi.util.PsiClassUtil;
+import com.intellij.psi.util.PsiUtil;
+import com.intellij.util.LocalTimeCounter;
+import com.intellij.util.ResourceUtil;
 import com.intellij.util.ui.UIUtil;
+import com.wdf.fudoc.FuDocTest;
 import com.wdf.fudoc.apidoc.sync.dto.ApiProjectDTO;
 import com.wdf.fudoc.apidoc.view.dialog.SyncApiCategoryDialog;
 import com.wdf.fudoc.common.enumtype.FuColor;
 import com.wdf.fudoc.components.message.FuMessageComponent;
 import com.wdf.fudoc.components.message.FuMsgBuilder;
+import com.wdf.fudoc.util.EditorUtils;
 import com.wdf.fudoc.util.FuDocUtils;
 import com.wdf.fudoc.util.PopupUtils;
 import com.wdf.fudoc.util.PsiClassUtils;
 import org.jetbrains.annotations.NotNull;
 
+import java.io.File;
 import java.util.concurrent.atomic.AtomicBoolean;
 
 /**
@@ -25,10 +49,36 @@ public class TestTableAction extends AnAction {
 
     @Override
     public void actionPerformed(@NotNull AnActionEvent e) {
-        FuMessageComponent fuMessageComponent = new FuMessageComponent();
-        fuMessageComponent.setForeground(UIUtil.getLabelFontColor(UIUtil.FontColor.BRIGHTER));
-        UIUtil.applyStyle(UIUtil.ComponentStyle.SMALL, fuMessageComponent);
-        fuMessageComponent.setMsg(FuMsgBuilder.getInstance().text("Size: ").text("806 B", FuColor.GREEN).build());
-        PopupUtils.create(fuMessageComponent,fuMessageComponent,new AtomicBoolean(false));
+        EditorFactory editorFactory = EditorFactory.getInstance();
+        PsiElement targetElement = PsiClassUtils.getTargetElement(e);
+        PsiClass psiClass = PsiClassUtils.getPsiClass(targetElement);
+        PsiFile containingFile = psiClass.getContainingFile();
+//        Document document = PsiDocumentManager.getInstance(e.getProject()).getDocument(containingFile);
+//        PsiClass aClass = JavaDirectoryService.getInstance().createClass(dir, "a", "Class", true);
+//        PsiDirectoryFactory.getInstance(e.getProject()).createDirectory()
+//        JavaDirectoryService.getInstance().createClass();
+        String text = "package com.wdf.fudoc;\n" +
+                "\n" +
+                "/**\n" +
+                " * @author wangdingfu\n" +
+                " * @date 2023-02-10 22:16:48\n" +
+                " */\n" +
+                "public class FuDocTest {\n" +
+                "\n" +
+                "    public static void main(String[] args) {\n" +
+                "        System.out.println(\"222\");\n" +
+                "    }\n" +
+                "\n" +
+                "    public void authAfter(String userName, String password) {\n" +
+                "\n" +
+                "    }\n" +
+                "}\n";
+        PsiFile file = PsiFileFactory.getInstance(e.getProject()).createFileFromText("sample.java", JavaFileType.INSTANCE, text, LocalTimeCounter.currentTime(), false, false);
+        file.putUserData(PsiUtil.FILE_LANGUAGE_LEVEL_KEY, LanguageLevel.HIGHEST);
+        Document document = PsiDocumentManager.getInstance(e.getProject()).getDocument(file);
+
+        Editor editor = editorFactory.createEditor(document, e.getProject(), JavaFileType.INSTANCE, false);
+        PopupUtils.create(editor.getComponent(),null,new AtomicBoolean(true));
+
     }
 }
