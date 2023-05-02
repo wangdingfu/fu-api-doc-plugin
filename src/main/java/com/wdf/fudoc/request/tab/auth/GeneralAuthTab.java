3415d7ab4dac5199494bda4f310a32ba49bb8161
wangdingfu <wangdingfu1024@163.com>
2023-05-02 21:03:04 +0800

    新增鉴权配置UI tab

---
+++ src/main/java/com/wdf/fudoc/request/tab/auth/GeneralAuthTab.java
@@ -0,0 +1,19 @@
+package com.wdf.fudoc.request.tab.auth;
+
+import com.intellij.ui.tabs.TabInfo;
+import com.wdf.fudoc.common.FuTab;
+
+/**
+ * 普通授权tab
+ *
+ * @author wangdingfu
+ * @date 2023-05-02 18:50:53
+ */
+public class GeneralAuthTab implements FuTab {
+
+
+    @Override
+    public TabInfo getTabInfo() {
+        return null;
+    }
+}
