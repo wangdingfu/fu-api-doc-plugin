504ab51d5545db04839b443b142cf6f80ab5c11f
王定福 <wangdingfu1024@163.com>
2023-07-23 17:53:27 +0800

    细节问题修复 统一Spring环境 流程优化

--- src/main/java/com/wdf/fudoc/request/tab/settings/GlobalConfigTab.java
+++ src/main/java/com/wdf/fudoc/request/tab/settings/GlobalConfigTab.java
@@ -1,11 +1,9 @@
 package com.wdf.fudoc.request.tab.settings;
 
 import com.google.common.collect.Lists;
-import com.intellij.icons.AllIcons;
 import com.intellij.ui.IdeBorderFactory;
 import com.intellij.ui.components.panels.VerticalBox;
 import com.intellij.ui.tabs.TabInfo;
-import com.intellij.util.ui.JBUI;
 import com.wdf.fudoc.common.FuBundle;
 import com.wdf.fudoc.common.FuDataTab;
 import com.wdf.fudoc.common.constant.MessageConstants;
